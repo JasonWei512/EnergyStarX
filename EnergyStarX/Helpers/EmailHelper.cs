112117067910b8a7d2319801ac940c1b00195e52
魏杰 <16899918+JasonWei512@users.noreply.github.com>
2022-09-28 12:39:39 +0800

    Add EmailHelper

---
+++ EnergyStarX/Helpers/EmailHelper.cs
@@ -0,0 +1,17 @@
+using Flurl;
+using Windows.System; 
+
+namespace EnergyStarX.Helpers;
+
+// UWP's EmailManager is not supported in WinUI 1.1
+// https://github.com/microsoft/microsoft-ui-xaml/issues/7300
+public static class EmailHelper
+{
+    private const string MailProtocolPrefix = "mailto:";
+
+    public static async Task SendEmail(string address, string subject, string body)
+    {
+        Uri mailUri = new Url(MailProtocolPrefix + address).SetQueryParams(new { subject = subject, body = body}).ToUri();
+        await Launcher.LaunchUriAsync(mailUri);
+    }
+}
