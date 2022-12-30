f1af41618d01eb3b5760429d389634f7839346c1
JasonWei512 <16899918+JasonWei512@users.noreply.github.com>
2022-12-30 19:56:28 +0800

    Report package version in App Center event

---
+++ EnergyStarX/Helpers/PackageInfo.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Windows.ApplicationModel;
+
+namespace EnergyStarX.Helpers;
+
+public class PackageInfo
+{
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    private static extern int GetCurrentPackageFullName(ref int packageFullNameLength, StringBuilder? packageFullName);
+
+    public static bool IsMSIX
+    {
+        get
+        {
+            int length = 0;
+
+            return GetCurrentPackageFullName(ref length, null) != 15700L;
+        }
+    }
+
+    public static Version Version
+    {
+        get
+        {
+            Version version;
+
+            if (IsMSIX)
+            {
+                PackageVersion packageVersion = Package.Current.Id.Version;
+                version = new Version(packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision);
+            }
+            else
+            {
+                version = Assembly.GetExecutingAssembly().GetName().Version!;
+            }
+
+            return version;
+        }
+    }
+
+    public static string VersionString
+    {
+        get
+        {
+            Version version = Version;
+            return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+    }
+}
