3eda07293a0135c7f453aa1b3ebd22b5b676057c
JasonWei512 <16899918+JasonWei512@users.noreply.github.com>
2023-02-25 09:22:17 +0800

    Fix naming

--- EnergyStarX/Helpers/SettingsHelper.cs
+++ EnergyStarX/Helpers/SettingsHelper.cs
@@ -12,11 +12,11 @@ namespace EnergyStarX.Helpers;
 
 public static class SettingsHelper
 {
-    private static IPropertySet localSettings => ApplicationData.Current.LocalSettings.Values;
+    private static IPropertySet LocalSettings => ApplicationData.Current.LocalSettings.Values;
 
     private static T GetSettingPrivate<T>(string settingKey, T defaultValue)
     {
-        if (localSettings.TryGetValue(settingKey, out object? settingObject))
+        if (LocalSettings.TryGetValue(settingKey, out object? settingObject))
         {
             if (settingObject is T settingValue)
             {
@@ -30,70 +30,70 @@ public static class SettingsHelper
 
     public static int GetSetting(string settingKey, int defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, int value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, int value) => LocalSettings[settingKey] = value;
 
 
     // long
 
     public static long GetSetting(string settingKey, long defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, long value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, long value) => LocalSettings[settingKey] = value;
 
 
     // float
 
     public static float GetSetting(string settingKey, float defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, float value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, float value) => LocalSettings[settingKey] = value;
 
 
     // double
 
     public static double GetSetting(string settingKey, double defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, double value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, double value) => LocalSettings[settingKey] = value;
 
 
     // bool
 
     public static bool GetSetting(string settingKey, bool defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, bool value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, bool value) => LocalSettings[settingKey] = value;
 
 
     // char
 
     public static char GetSetting(string settingKey, char defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, char value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, char value) => LocalSettings[settingKey] = value;
 
 
     // string
 
     public static string GetSetting(string settingKey, string defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, string value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, string value) => LocalSettings[settingKey] = value;
 
 
     // DateTime
 
     public static DateTime GetSetting(string settingKey, DateTime defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, DateTime value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, DateTime value) => LocalSettings[settingKey] = value;
 
 
     // TimeSpan
 
     public static TimeSpan GetSetting(string settingKey, TimeSpan defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, TimeSpan value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, TimeSpan value) => LocalSettings[settingKey] = value;
 
 
     // Guid
 
     public static Guid GetSetting(string settingKey, Guid defaultValue) => GetSettingPrivate(settingKey, defaultValue);
 
-    public static void SetSetting(string settingKey, Guid value) => localSettings[settingKey] = value;
+    public static void SetSetting(string settingKey, Guid value) => LocalSettings[settingKey] = value;
 
 
 }
