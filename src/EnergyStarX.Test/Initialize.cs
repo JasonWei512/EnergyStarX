76cc66058c11e4d799ff9c003cb8bd3b2cc37733
JasonWei512 <16899918+JasonWei512@users.noreply.github.com>
2023-04-20 20:27:07 +0800

    Add unit test project

---
+++ src/EnergyStarX.Test/Initialize.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Windows.ApplicationModel.DynamicDependency;
+
+[assembly: WinUITestTarget(typeof(EnergyStarX.App))]
+
+namespace EnergyStarX.Test;
+
+[TestClass]
+public class Initialize
+{
+    [AssemblyInitialize]
+    public static void AssemblyInitialize(TestContext context)
+    {
+        // TODO: Initialize the appropriate version of the Windows App SDK.
+        // This is required when testing MSIX apps that are framework-dependent on the Windows App SDK.
+        Bootstrap.TryInitialize(0x00010001, out int _);
+    }
+
+    [AssemblyCleanup]
+    public static void AssemblyCleanup()
+    {
+        Bootstrap.Shutdown();
+    }
+}
