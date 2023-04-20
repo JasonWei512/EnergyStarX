f7af01f31d081cd9065d52febdb7378659ca201b
JasonWei512 <16899918+JasonWei512@users.noreply.github.com>
2023-04-20 20:27:07 +0800

    Add unit test project

---
+++ src/EnergyStarX.Test/TestClass.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace EnergyStarX.Test;
+
+// TODO: Write unit tests.
+// https://docs.microsoft.com/visualstudio/test/getting-started-with-unit-testing
+// https://docs.microsoft.com/visualstudio/test/using-microsoft-visualstudio-testtools-unittesting-members-in-unit-tests
+// https://docs.microsoft.com/visualstudio/test/run-unit-tests-with-test-explorer
+
+[TestClass]
+public class TestClass
+{
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext context)
+    {
+        Debug.WriteLine("ClassInitialize");
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        Debug.WriteLine("ClassCleanup");
+    }
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        Debug.WriteLine("TestInitialize");
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        Debug.WriteLine("TestCleanup");
+    }
+
+    [TestMethod]
+    public void TestMethod()
+    {
+        Assert.IsTrue(true);
+    }
+
+    // UITestMethod doesn't work correctly with Windows App SDK 1.3
+    //[UITestMethod]
+    //public void UITestMethod()
+    //{
+    //    Assert.AreEqual(0, new Grid().ActualWidth);
+    //}
+}
